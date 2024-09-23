93a50b75d1fcda416065dbe90ab1c301303df345
Marcelo Javier Aguila <marceloag@Mac-mini-de-Marcelo.local>
2024-09-23 16:51:59 -0300

    typo

--- page.php
+++ page.php
@@ -1,6 +1,6 @@
 <?php get_header(); ?>
 
-	  <section>
+	  <section class="maincontent">
 			<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
         <div class="flex flex-row items-center justify-center p-4" id="interior">
             <div class="w-11/12 md:w-9/12">
